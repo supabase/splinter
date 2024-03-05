95358deb333d420f83340db7224ff560dc88118b
Oliver Rice <github@oliverrice.com>
2024-03-05 14:31:13 -0600

    add lint for unused indexes

---
+++ test/sql/0005_unused_index.sql
@@ -0,0 +1,19 @@
+begin;
+
+	-- No issues	
+	select * from lint."0005_unused_index";
+
+    create table public.foo (
+        id int primary key,
+        foo text unique,
+        bar text
+    );
+
+    create index some_unused_index on public.foo (bar);
+
+    analyze public.foo;
+
+    -- Only the "bar" table is listed
+	select * from lint."0005_unused_index";
+
+rollback;
