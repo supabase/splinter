3fcd4cf3c12c86e6a11459acf2c2cf6e65880f30
Oliver Rice <github@oliverrice.com>
2024-04-01 13:40:44 -0500

    add lint for duplicate indexes

---
+++ docs/0009_duplicate_index.md
@@ -0,0 +1,20 @@
+
+Level: INFO 
+
+### Rationale
+
+Each index in a Postgres database adds overhead. This overhead occurs because the database must update each index whenever data in the indexed table are inserted, updated, or deleted. If two or more indexes are exact duplicates in their composition, the database incurs additional write overhead for no performance benefit.
+
+### What is an Index?
+
+An index in a database is similar to an index in a book. It allows the database to find data without scanning the entire table. An index is created on a column or a set of columns in a table. Queries that search or sort data based on these columns can find data more quickly and efficiently by referring the index instead of each row in the table.
+
+### How to Resolve
+
+When a table contains a duplicate index, drop instances of the index until only one remains.
+
+For example, if the table `public.blog` has duplicate indexes `public.ix_id_1` and `public.ix_id_2` drop one using:
+
+```sql
+drop index public.ix_id_2;
+```
--- splinter.sql
+++ splinter.sql
@@ -414,3 +414,54 @@ where
 group by
     n.nspname,
     c.relname)
+union all
+(
+select
+    'duplicate_index' as name,
+    'WARN' as level,
+    'EXTERNAL' as facing,
+    'Detects cases where two ore more identical indexes exist.' as description,
+    format(
+        'Table \`%s.%s\` has identical indexes %s. Drop all except one of them',
+        n.nspname,
+        c.relname,
+        array_agg(pi.indexname order by pi.indexname)
+    ) as detail,
+    'https://supabase.github.io/splinter/0009_duplicate_index' as remediation,
+    jsonb_build_object(
+        'schema', n.nspname,
+        'name', c.relname,
+        'type', case
+            when c.relkind = 'r' then 'table'
+            when c.relkind = 'm' then 'materialized view'
+            else 'ERROR'
+        end,
+        'indexes', array_agg(pi.indexname order by pi.indexname)
+    ) as metadata,
+    format(
+        'duplicate_index_%s_%s_%s',
+        n.nspname,
+        c.relname,
+        array_agg(pi.indexname order by pi.indexname)
+    ) as cache_key
+from
+    pg_indexes pi
+    join pg_catalog.pg_namespace n
+        on n.nspname  = pi.schemaname
+    join pg_catalog.pg_class c
+        on pi.tablename = c.relname
+        and n.oid = c.relnamespace
+    left join pg_catalog.pg_policy p
+        on p.polrelid = c.oid
+where
+    c.relkind in ('r', 'm') -- tables and materialized views
+    and n.nspname not in (
+        'pg_catalog', 'information_schema', 'auth', 'storage', 'vault', 'pgsodium'
+    )
+group by
+    n.nspname,
+    c.relkind,
+    c.relname,
+    replace(pi.indexdef, pi.indexname, '')
+having
+    count(*) > 1)
