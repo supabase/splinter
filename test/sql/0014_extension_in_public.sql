162a2a7a64b650389f5ab19fb21ddec3fdc21f79
Oliver Rice <github@oliverrice.com>
2024-04-12 13:30:05 -0500

    Add lint for extensions in public schema (#45)
    * add lint for extensions in public schema
    
    * sync test output

---
+++ test/sql/0014_extension_in_public.sql
@@ -0,0 +1,17 @@
+begin;
+
+  -- 0 issues
+  select * from lint."0014_extension_in_public";
+
+  create extension ltree schema public;
+
+  -- 1 issues
+  select * from lint."0014_extension_in_public";
+
+  -- resolve the problem
+  alter extension ltree set schema extensions;
+  
+  -- 0 issues
+  select * from lint."0014_extension_in_public";
+
+rollback;
