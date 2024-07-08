f2ffd77420b8a0abae965bfaf6d2bcc0be50de93
Oliver Rice <github@oliverrice.com>
2024-07-08 10:27:19 -0500

    enable 0003 auth_rls_initplan check on realtime schema (#88)

--- test/sql/0003_auth_rls_initplan.sql
+++ test/sql/0003_auth_rls_initplan.sql
@@ -1,6 +1,8 @@
 begin;
     set local search_path = '';
 
+    savepoint a;
+
     -- No issues
     select * from lint."0003_auth_rls_initplan";
 
@@ -60,6 +62,22 @@ begin;
     alter table public.foo enable row level security;
 
     -- 4 entries, 1 per "bad_" policy
+    select * from lint."0003_auth_rls_initplan";
+
+    rollback to savepoint a;
+
+    -- Confirm that realtime.messages will be scanned
+    create schema realtime;
+    create table realtime.messages( id int primary key);
+
+    create policy realtime_check on realtime.messages
+    for insert
+        to authenticated
+        with check ('foo' = auth.role());
+
+    alter table realtime.messages enable row level security;
+
+    -- 1 entry for realtime.messages
     select * from lint."0003_auth_rls_initplan";
 
 rollback;
